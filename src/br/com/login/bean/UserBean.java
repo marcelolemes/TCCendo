02a3c2cd6e905e21b72d4c74f0f836c849734e6b
marcelolemes <lemmarcelo@gmail.com>
2014-09-15 07:20:53 -0300

    Primeiro commit do novo branch

---
+++ src/br/com/login/bean/UserBean.java
@@ -0,0 +1,222 @@
+package br.com.login.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+
+import br.com.login.Dao.UserDao;
+import br.com.login.model.Metricas;
+import br.com.login.model.User;
+
+@ManagedBean(name = "userBean", eager = true)
+@SessionScoped
+public class UserBean implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<SelectItem> nivelAcessoCadastro = new Metricas()
+			.getNivelAcesso();
+
+	public UserBean() {
+
+		user = new User();
+		user.setLogado(false);
+		
+	}
+
+	private User user;
+	private User userLogado;
+	UserDao userDao;
+	private String sessao = new String();
+
+	private static boolean logado = false;
+
+	public User getUser() {
+		return user;
+
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String atualizar() throws Exception {
+
+		userLogado = userDao.atualizar(userLogado);
+		// return "result.xhtml";
+		return "/pages/result_index.xhtml";
+	}
+
+	public String logar() throws Exception {
+		if (user.isLogado()) {
+			loginAtivo();
+						
+			return "/pages/result_index.xhtml";
+
+		} else {
+			userDao = new UserDao();
+			if ((userLogado = userDao.testarLogin(user)) != null) {
+				System.out.print(" Encontrado ");
+				setSessao(user.getApelido());
+				user = new User();
+				user.setLogado(true);
+				return messageSucessoLogin();
+
+			} else {
+				System.out.print("Não encontrado");
+				user = new User();
+				user.setLogado(false);
+				messageErroLogin();
+			}
+			return null;
+		}
+	}
+	public String verificarLogado() throws Exception {
+
+		if (user.isLogado() /*&& (userBean.getUserLogado() != null)*/) {
+
+			return "/pages/result_index.xhtml";
+			
+		} else {
+			return "/pages/login_index.xhtml";
+		}
+	}
+	
+	
+	public String gravar() {
+		userDao = new UserDao();
+		try {
+			if (userDao.Gravar(user)) {
+				messageSucessoGravar();
+				user = new User();
+				user.setLogado(true);
+			}
+			return "/pages/result_index.xhtml";
+
+		} catch (Exception ex) {
+			messageErroCadastro();
+			ex.printStackTrace();
+			ex.getMessage();
+			return null;
+		}
+
+	}
+
+	public String messageSucessoLogin() {
+		FacesContext.getCurrentInstance().addMessage(
+				null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Login",
+						"Seja bem vindo " + sessao));
+		// return "result.xhtml";
+		return "/pages/result_index.xhtml";
+
+	}
+
+	public String loginAtivo() {
+		FacesContext.getCurrentInstance().addMessage(
+				null,
+				new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Ativo",
+						"Sessão ainda ativa para o usuário:  " + sessao));
+		// return "result.xhtml";
+		return "/pages/result_index.xhtml";
+
+	}
+	
+	public String reiniciarsessão() {
+		FacesContext.getCurrentInstance().addMessage(
+				null,
+				new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Ativo",
+						"Sessão ativa reiniciada:  " + sessao));
+		// return "result.xhtml";
+		return "/pages/result_index.xhtml";
+
+	}
+
+	public void messageSucessoGravar() {
+		FacesContext.getCurrentInstance().addMessage(
+				null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Gravar",
+						"Cadastro realizado com sucesso, Seja bem vindo "
+								+ sessao));
+
+	}
+
+	public void messageErroLogin() {
+		FacesContext
+				.getCurrentInstance()
+				.addMessage(
+						null,
+						new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login",
+								"Usuário/Senha incorretos, por favor, tente novamente"));
+		// remover sessão do manage bean selecionado
+//		FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
+//				.remove("userBean");
+	}
+
+	public void nenhumUsuario() {
+		FacesContext
+				.getCurrentInstance()
+				.addMessage(
+						null,
+						new FacesMessage(FacesMessage.SEVERITY_FATAL, "Erro",
+								"Nenhum usuário logado, para realizar alguma operação, efetue seu login"));
+
+	}
+
+	public void autoridadeInsuficiente() {
+		FacesContext
+				.getCurrentInstance()
+				.addMessage(
+						null,
+						new FacesMessage(FacesMessage.SEVERITY_WARN, userLogado
+								.getApelido(),
+								"Seu acesso à esta função não é permitida, acesso negado!"));
+
+	}
+
+	public void messageErroCadastro() {
+		FacesContext.getCurrentInstance().addMessage(
+				null,
+				new FacesMessage(FacesMessage.SEVERITY_ERROR, "Cadastro",
+						"O Cadastro falhou, por favor, tente novamente"));
+	}
+
+	public String getSessao() {
+		return sessao;
+	}
+
+	public void setSessao(String sessao) {
+		this.sessao = sessao;
+	}
+
+	public User getUserLogado() {
+		return userLogado;
+	}
+
+	public void setUserLogado(User userLogado) {
+		this.userLogado = userLogado;
+	}
+
+	public boolean isLogado() {
+		return logado;
+	}
+
+	public void setLogado(boolean logado) {
+		UserBean.logado = logado;
+	}
+
+	public List<SelectItem> getNivelAcessoCadastro() {
+		return nivelAcessoCadastro;
+	}
+
+	public void setNivelAcessoCadastro(List<SelectItem> nivelAcessoCadastro) {
+		this.nivelAcessoCadastro = nivelAcessoCadastro;
+	}
+
+}
---
+++ src/br/com/login/model/User.java
@@ -0,0 +1,113 @@
+package br.com.login.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import javax.faces.bean.ManagedBean;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@ManagedBean
+@Table(name = "Usuario")
+public class User implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int cod;
+	@Column
+	private int nivelacesso;
+	@Column
+	private String apelido;
+	@Column
+	private String senha;
+	@Column
+	private String servicoatual;
+	@Column
+	private String servicoanterior;
+	@Column
+	private Timestamp ultimoacesso;
+	@Column
+	private boolean logado;
+
+	public Timestamp getUltimoacesso() {
+		return ultimoacesso;
+	}
+
+	public void setUltimoacesso(Timestamp ultimoacesso) {
+		this.ultimoacesso = ultimoacesso;
+	}
+
+	public String getServicoanterior() {
+		return servicoanterior;
+	}
+
+	public void setServicoanterior(String servicoanterior) {
+		this.servicoanterior = servicoanterior;
+	}
+
+	public String getServicoatual() {
+		return servicoatual;
+	}
+
+	public void setServicoatual(String servicoatual) {
+		this.servicoatual = servicoatual;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+	public int getNivelAcesso() {
+		return nivelacesso;
+	}
+
+	public void setNivelAcesso(int nivelAcesso) {
+		this.nivelacesso = nivelAcesso;
+	}
+
+	public String getApelido() {
+		return apelido;
+	}
+
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public int getNivelacesso() {
+		return nivelacesso;
+	}
+
+	public void setNivelacesso(int nivelacesso) {
+		this.nivelacesso = nivelacesso;
+	}
+
+	public boolean isLogado() {
+		return logado;
+	}
+
+	public void setLogado(boolean logado) {
+		this.logado = logado;
+	}
+
+}
